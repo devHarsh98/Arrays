6788d570d9154273fa491baf73476e93a2a361b5
devHarsh98 <harshchauhan8082@gmail.com>
2023-11-07 18:06:23 +0530

    linearSearch

---
+++ 1.linearSearch.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int linearSearch(int arr[], int key, int n) {
+    for(int i=0; i<n; i++) {
+        if(arr[i] == key) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {2, 3, 4, 10, 40};
+    int key = 10;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = linearSearch(arr,key,n);
+    if(result == -1) {
+        cout << "Element is not present in the array";
+    }
+    else {
+        cout << "Element is present at index "<<result;
+    }
+    return 0;
+}
